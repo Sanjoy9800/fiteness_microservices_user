b1d13fb5d7868a8a07a037bff4be6a93ccb0a32b
Sanjoy9800 <sanjurudra4@gmail.com>
2026-06-23 09:30:06 +0530

    "Updated push"

--- userservice/src/main/java/com/fitness/userservice/UserRepository.java
+++ userservice/src/main/java/com/fitness/userservice/UserRepository.java
@@ -4,6 +4,7 @@ import com.fitness.userservice.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//test
 @Repository
 public interface UserRepository extends JpaRepository<User, String> {
     Boolean existsByEmail(String email);
